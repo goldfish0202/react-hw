14ba6e86352e574d3a8b4d36266f2f0f74629933
goldfish0202 <cx8133@gmail.com>
2021-04-15 09:31:33 +0800

    0408hw

---
+++ hw/0408/App1.js
@@ -0,0 +1,58 @@
+import './App.css';
+import React from 'react';
+class FormatDate extends React.Component {
+  constructor(props){
+    super(props);
+  }
+	render() {
+		return this.props.date.toLocaleDateString();
+	}
+}
+
+class Avatar extends React.Component {
+  constructor(props){
+    super(props);
+  }
+  render() {
+		return (
+      <img
+        className="Avatar"
+        src={this.props.user.avatarUrl}
+        alt={this.props.user.name}
+      />
+    );
+	}
+}
+
+class UserInfo extends React.Component {
+  constructor(props){
+    super(props);
+  }
+  render() {
+    return (
+      <div className="UserInfo">
+        <Avatar user={this.props.user} />
+        <div className="UserInfo-name">{this.props.user.name}</div>
+      </div>
+    );
+  }
+}
+
+class App extends React.Component {
+  constructor(props){
+    super(props);
+  }
+  render() {
+    return (
+      <div className="Comment">
+        <UserInfo user={this.props.author} />
+        <div className="Comment-text">{this.props.text}</div>
+        <div className="Comment-date">
+          <FormatDate date = {this.props.date}/>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
